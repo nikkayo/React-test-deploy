22971fbec20f369ea07cdfa589c43353a17f65b6
Nikkayowill <w0439161@nscc.ca>
2026-02-03 13:54:37 -0400

    Lets do this test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>The TEST WORKED, but does it work this time??!</h1>
+      <h1>The TEST WORKED, or did it?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
