73a89e1a64753feb32a85490dfd0010646ab5feb
Nikkayowill <w0439161@nscc.ca>
2026-02-05 00:11:21 -0400

    Test Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>The TEST WORKED, or did it?</h1>
+      <h1>The TEST WORKED, idk man, Lebron James</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
